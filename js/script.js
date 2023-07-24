e3a2a3233c52b3702960a87f2334f7404fd1c22a
Ifeoluwa <ifeoluwaadeleke3008@gmail.com>
2023-07-24 12:22:04 +0100

    portfolio-template

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//typing animation
+var typed = new Typed(".typing", {
+    strings:["", "Frontend Developer","Web Developer", "Web Designer","Graphics Designer"],
+    typeSpeed: 100,
+    BackSpeed:60,
+    loop:true
+})
